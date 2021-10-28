972f4e0cc1f8c2de6c32cb8c6c0769fc55de46a6
Dekel Zoaretz <dekelzoaretz@gmail.com>
2021-10-28 09:04:04 +0300

    Create copyToClipboard.js

---
+++ copyToClipboard.js
@@ -0,0 +1,10 @@
+const copyToClipboard = (text) => {
+    const element = document.createElement('textarea');
+    element.value = text;
+    document.body.appendChild(element);
+    element.select();
+    document.execCommand('copy');
+    document.body.removeChild(element);
+};
+
+export { copyToClipboard };
